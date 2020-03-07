1e11113c14be69ffb77693e65079e80e22f55e1b
maciejkozikowski <maciej.kozik94@gmail.com>
2020-03-07 15:12:02 +0100

    toolbar

---
+++ front/app/src/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Measurements from './views/Measurements.vue'
+import Parameters from './views/Parameters.vue'
+import Statistics from './views/Statistics.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'measurements',
+      component: Measurements
+    },
+    {
+      path: '/parameters',
+      name: 'parameters',
+      component: Parameters
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: Statistics
+    }
+
+  ]
+})
---
+++ front/app/src/views/Measurements.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="measurements">
+        <h1>MeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurementsMeasurements</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'measurements'
+}
+</script>
