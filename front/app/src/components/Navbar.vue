55a777f3e367f4a6910e4c6983c1bf2e000cfad7
maciejkozikowski <maciej.kozik94@gmail.com>
2020-03-07 15:12:02 +0100

    toolbar

---
+++ front/app/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav>
+        <v-toolbar>
+        <v-toolbar-title>
+            <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-btn>{{ link.text }}</v-btn>
+          </v-list-tile-action>
+          <v-list-tile-content>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+        </v-toolbar-title> 
+        </v-toolbar>
+
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+    return {
+      links: [
+        { text: 'Measurements', route: '/' },
+        { text: 'Parameters', route: '/parameters' },
+        { text: 'Statistics', route: '/statistics' },
+      ]
+    }
+    }
+}
+</script>
---
+++ front/app/src/views/Parameters.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="parameters">
+        <h1>Parameters</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'parameters'
+}
+</script>
